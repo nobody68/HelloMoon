4c215340f929c7a1739be8c31d30089bde00e82c
salmatador <salmatador@moonstub.com>
2015-10-15 08:55:28 -0700

    Initial commit

---
+++ app/src/main/java/com/example/training/app/hellomoon/MoonActivity.java
@@ -0,0 +1,13 @@
+package com.example.training.app.hellomoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moon);
+    }
+}
